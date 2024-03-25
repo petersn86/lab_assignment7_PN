1dae4070fb6b7488cd854da4eb72a2357e16427c
petersn86 <133061453+petersn86@users.noreply.github.com>
2024-03-25 19:23:56 -0400

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,14 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-        {/* <AcmeLogo /> */}
-      </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 	<div
   className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent"
 />
           <p className={`${lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Cool Dogs.</strong> This is the place to find pictures of cool dogs{' '}
+            , brought to you by Peter Nolan.
           </p>
           <Link
             href="/login"
@@ -35,14 +28,14 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/cool_dog1.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/hero-mobile.png"
+            src="/cool_dog1.png"
             width={560}
             height={620}
             className="block md:hidden"
